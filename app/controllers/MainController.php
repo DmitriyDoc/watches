228656755ed3e45ac5254dc98f7083ca9f50a1fa
Dmitriy <f.doc2@yandex.ru>
2020-10-25 20:46:52 +0300

    Вывод брендов и создание БД

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,6 +10,9 @@ class MainController extends AppController
 {
 
     public function indexAction(){
+        $brands = \R::find('brand','LIMIT 3'); // findAll()
+
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 }
