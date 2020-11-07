51115bca35b9ee00fc2dae4348430893cb135c0b
Dmitriy <f.doc2@yandex.ru>
2020-11-07 21:31:21 +0300

    Карточка товара. Картинки галереи

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -24,11 +24,12 @@ class ProductController extends AppController
         // просмотренные товары из кук
 
         // галерея
+        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
 
         // модификации
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related'));
+        $this->set(compact('product', 'related', 'gallery'));
     }
 
 }
